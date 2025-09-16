c8743e8cdbecd74e2e2b7524af74808a4bb53e57
dschifel <danielschifelbain@gmail.com>
2025-09-16 18:46:35 -0300

    Refac/Feat: renamed testing assets / add fade trigger

--- src/components/window/window.component.ts
+++ src/components/window/window.component.ts
@@ -11,18 +11,34 @@ import { Project } from '../../models/Project';
 })
 export class WindowComponent {
   projects: Project[] = [
-    {name: 'Project 1', image: 'assets/img/projects/project1.png', likes: 10, comments: 5},
-    {name: 'Project 2', image: 'assets/img/projects/project2.png', likes: 20, comments: 15},
-    {name: 'Project 3', image: 'assets/img/projects/project3.png', likes: 30, comments: 25}
+    {name: 'Project 1', image: 'assets/img/projects/project1_lg.png', likes: 10, comments: 5},
+    {name: 'Project 2', image: 'assets/img/projects/project2_lg.png', likes: 20, comments: 15},
+    {name: 'Project 3', image: 'assets/img/projects/project3_lg.png', likes: 30, comments: 25}
   ];
 
   currentIndex: number = 0;
+  fade: boolean = false;
 
   nextProject() {
-    this.currentIndex = (this.currentIndex + 1) % this.projects.length;
+    this.triggerFade(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.projects.length;
+    });
   }
 
   prevProject() {
-    this.currentIndex = (this.currentIndex - 1 + this.projects.length) % this.projects.length;
+    this.triggerFade(() => {
+      this.currentIndex = (this.currentIndex - 1 + this.projects.length) % this.projects.length;
+    });
+  }
+
+  private triggerFade(callback: () => void): void {
+    this.fade = true;
+
+    setTimeout(() => {
+      callback();
+      this.fade = false;
+    }, 500);
   }
 }
+
+
