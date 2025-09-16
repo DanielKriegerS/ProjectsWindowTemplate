58758a98533c6a5fc3298d7af80cc33aecadc593
dschifel <danielschifelbain@gmail.com>
2025-09-16 17:22:33 -0300

    Feat: implemented basic project and mocked testing project

--- src/components/window/window.component.ts
+++ src/components/window/window.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { Project } from '../../models/Project';
 
 @Component({
   selector: 'app-window',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './window.component.html',
-  styleUrl: './window.component.scss'
+  styleUrls: ['./window.component.scss']
 })
 export class WindowComponent {
+  projects: Project[] = [
+    {name: 'Project 1', image: 'assets/img/projects/project1.png', likes: 10, comments: 5},
+    {name: 'Project 2', image: 'assets/img/projects/project2.png', likes: 20, comments: 15},
+    {name: 'Project 3', image: 'assets/img/projects/project3.png', likes: 30, comments: 25}
+  ];
 
+  currentIndex: number = 0;
+
+  nextProject() {
+    this.currentIndex = (this.currentIndex + 1) % this.projects.length;
+  }
+
+  prevProject() {
+    this.currentIndex = (this.currentIndex - 1 + this.projects.length) % this.projects.length;
+  }
 }
